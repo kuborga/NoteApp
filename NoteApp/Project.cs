4842f7301c87d39e9c0763124dc65bd0d6d81cf3
zwer.27@yandex.ru <zwer.27@yandex.ru>
2021-03-22 21:49:38 +0700

    Cоздан класс Project.Содержит список всех заметок.

---
+++ NoteApp/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Хранит список всех заметок
+    /// </summary>
+    class Project
+    {
+        /// <summary>
+        /// Возвращает список текущих заметок
+        /// </summary>
+        public List<Note> Notes { get; private set; } = new List<Note>();
+    }
+}
