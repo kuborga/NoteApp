17cf863114acc073167b6e67993f65dedf33627f
zwer.27@yandex.ru <zwer.27@yandex.ru>
2021-03-22 20:42:10 +0700

    Создание перечисления NoteCategory с комментариями

---
+++ NoteApp/NoteCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Категория заметок
+    /// </summary>
+    public enum  NoteCategory
+    {
+        Job,
+        Home,
+        HealthAndSports,
+        People,
+        Documents,
+        Finance,
+        Sundry  //(Разное)
+    }
+}
