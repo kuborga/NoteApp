e6027ee501ed232e799088a950519e307763844d
zwer.27@yandex.ru <zwer.27@yandex.ru>
2021-06-17 14:47:06 +0700

    Добавлена папка src и doc.Удалён из проекта NUnit3TestAdapter.

--- src/NoteAppUI/MainForm.cs
+++ src/NoteAppUI/MainForm.cs
@@ -33,7 +33,7 @@ namespace NoteAppUI
             RefreshListBox();
 
             //Выбирает последнюю просматреваемую заметку, если таковая существует в списке
-            if (NoteListBox.Items.Count > 0)
+            if (_viewedNotes.Count != 0)
             {
                 NoteListBox.SelectedIndex = _project.SelectedNoteIndex;
             }
@@ -225,7 +225,7 @@ namespace NoteAppUI
         /// <param name="e"></param>
         private void MainForm_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode.ToString() == "Delete")
+            if (e.KeyData == Keys.Delete)
             {
                 RemoveNote();
             }
